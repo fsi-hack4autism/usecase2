81224c7154bceaabdbbaa2ef6a411157d7aa1f61
Trishla <tms483@nyu.edu>
2022-04-01 16:33:33 -0400

    Remove db key

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     endpoint: "https://usecase2cosmosdbacct.documents.azure.com:443/",
-    key: "CuQMVMLbaPMw8Ut72NmaurZShQFocyJd4fa1roQzNtcedWglJRPydKbMfDEsmhNLQPhlsxLaAVaNnxT1JPa2ig==",
+    key: "",
     databaseId: "usecase2cosmoscontainer",
     containerId: "container1",
     partitionKey: { kind: "Hash", paths: ["/category"] }
